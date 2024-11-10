fc06b211b7c57a5757a8399f523152d2e19e9131
Paul Wijhenke <paul@paulsplace.nl>
2024-11-10 21:10:40 +0100

    feat/no unnecessary condition (#26)
    * new rule: typescript-eslint/no-unnecessary-condition
    
    * also simplified setup
    * also updated dependencies
    
    * v0.0.17

--- src/index.mjs
+++ src/index.mjs
@@ -6,60 +6,77 @@ import tseslint from "typescript-eslint"
 
 export {globals}
 
-// Our own prettier config
-const eslintRules = {
-	// utility
-	"no-console": [
-		"error",
-		{
-			allow: ["info", "warn", "error", "debug"],
-		},
-	],
-	"require-await": "error",
-
-	// readability
-	"no-trailing-spaces": "error",
-	curly: "error",
-
-	// simple-import-sort plugin
-	"simple-import-sort/exports": "error",
-	"simple-import-sort/imports": "error",
-}
-
-// Overrides for the prettier config
-const eslintPrettierOverride = {
-	"no-multiple-empty-lines": [
-		"error",
-		{
-			max: 2,
-		},
-	],
-	"key-spacing": [
-		"error",
-		{
-			beforeColon: false,
-			afterColon: true,
-			mode: "strict",
-		},
-	],
-}
-
-// Export esLintConfig
-export const eslintConfig = [
+export const eslintConfig = tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
 	{
-		ignores: ["coverage/*", "dist/*"],
+		ignores: [
+			"node_modules",
+			"dist",
+			"coverage",
+			"prettier.config.js",
+			"eslint.config.js",
+			"vitest.*.ts",
+		],
 	},
+	// general rules
 	{
 		plugins: {
 			"simple-import-sort": simpleImportSort,
 		},
+		rules: {
+			// utility
+			"no-console": [
+				"error",
+				{
+					allow: ["info", "warn", "error", "debug"],
+				},
+			],
+			"require-await": "error",
+
+			// readability
+			"no-trailing-spaces": "error",
+			curly: "error",
+			"simple-import-sort/exports": "error",
+			"simple-import-sort/imports": "error",
+		},
 	},
-	eslint.configs.recommended,
-	...tseslint.configs.recommended,
-	{rules: eslintRules},
+	// TypeScript specific rules
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			ecmaVersion: 5,
+			sourceType: "script",
+			parserOptions: {
+				project: "./tsconfig.json",
+			},
+		},
+		rules: {
+			"@typescript-eslint/no-unnecessary-condition": "error",
+		},
+	},
+	// Prettier setup
 	eslintConfigPrettier,
-	{rules: eslintPrettierOverride},
-]
+	// Custom Prettier overrides
+	{
+		rules: {
+			"no-multiple-empty-lines": [
+				"error",
+				{
+					max: 2,
+				},
+			],
+			"key-spacing": [
+				"error",
+				{
+					beforeColon: false,
+					afterColon: true,
+					mode: "strict",
+				},
+			],
+		},
+	},
+)
 
 export const prettierConfig = {
 	arrowParens: "always",
